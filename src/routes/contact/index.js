55518693903c39b5ba5ee6ff78bd4f3dcfd29fbc
steven <github-deskop@gmail.com>
2022-09-27 15:41:42 +0700

    add website

--- src/routes/contact/index.js
+++ src/routes/contact/index.js
@@ -7,29 +7,22 @@ const photographs = (props) => {
 			<h1 class={style.pageTitle}>About me</h1>
 			<div class={style.formWrapper}>
 				<p class={style.pageBody}>
-					<div>
+					<p>
 						Mình là một frontend developer, có một thời gian làm việc ở FPT. Hiện mình đã nghỉ 
 						và có nguyện vọng hướng dẫn các bạn khiếm khuyết có mong muốn tìm hiểu về lập trình (web). 
-					</div>
-					<br />
-					<div>
+					</p>
+					<p>
 						Nếu các bạn có băn khoăn về tài chính, thì đừng quá lo lắng. Tất cả những gì các 
 						bạn cần là một máy tính kết nối và một sự yêu thích với công nghệ. Mình hi vọng giới thiệu với các bạn niềm vui của việc lập trình. 
 						Vì cá nhân mình tin kiến thức mang lại niềm vui, và kỹ năng lập trình cũng hữu ích trong thời đại này.
-					</div>
-					<br />
-					<div>
-						Ngoài ra nếu các bạn là những người mới bắt đầu tìm hiểu về lập trình (web), mình cũng có 
-						thể chia sẻ những gì mình biết. Hi vọng giúp được các bạn!!
-					</div>
-					<br />
-					<div>
-						Các bạn có thể mail cho mình qua địa chỉ - thikcafe13@gmail.com.
-					</div>
-					<br />
-					<div>
+					</p>
+					<p>
+						Các bạn có thể mail cho mình qua địa chỉ - trungtamkhoidau@gmail.com.
+					</p>
+					<p>Hay tham khảo thông tin các khoá học trên website - <a href='https://khoidau.netlify.app/'>https://khoidau.netlify.app/</a></p>
+					<p>
 						Hẹn gặp các bạn!!
-					</div>
+					</p>
 				</p>
 				{/* <form name="contact" method="POST" data-netlify="true" action="/contact/success" data-netlify-honeypot="bot-field">
 					<input type="hidden" name="form-name" value="contact" />
